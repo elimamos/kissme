2609a2719b112439d919d71c905775ebba2aaf7d
Aleksandra Pancer <pancer.aleksandra@gmail.com>
2017-12-06 14:02:51 +0100

    user interface LogIn

--- app/src/main/java/com/example/elisa/kissmekate/LogIn.java
+++ app/src/main/java/com/example/elisa/kissmekate/LogIn.java
@@ -2,6 +2,7 @@ package com.example.elisa.kissmekate;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class LogIn extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class LogIn extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log_in);
+    }
+
+    public void openCam(View view) {
     }
 }
